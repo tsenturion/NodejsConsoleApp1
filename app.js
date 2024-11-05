a0125788b1f8316e53e51db80981384756e41445
MariGA-93 <marina.muhamejanova@gmail.com>
2024-11-05 21:41:17 +0500

    05.11

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 console.log('Hello world');
+let secondName = "efimov";
+let name2 = 'matvey';
+let name3 = 'mikita';
+let age = 23;
+let upFlag = true;
+let downFlag = false;
+age = 10 % 4; // 2
+age = Math.pow(age, 4); // 2 ^ 4 = 16
+/*
+>
+<
+>=
+<=
+===
+!==
+
+*/
+age = 14;
+let maximumDataTransferSpeed = 23; // 1
+let dayOfWeek = "monday"; // 2
+let result = 2 + 6 / (1 + 2); // 3
+console.log('12 %d', result);
+if (upFlag) {
+    console.log(upFlag);
+}
+else {
+    console.log('���� ��������');
+}
+const readline = require("readline");
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let name4 = '5';
+r1.question('33', (name) => {
+    name4 = name;
+    console.log(name4);
+    r1.close();
+});
+console.log('23', name4);
+process.stdin.resume();
 process.stdin.resume();
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,2 +1,44 @@
 console.log('Hello world');
+let secondName: string = "efimov";
+let name2: string = 'matvey';
+let name3: string = 'mikita';
+let age: number = 23;
+let upFlag: boolean = true;
+let downFlag: boolean = false;
+age = 10 % 4; // 2
+age = age ** 4; // 2 ^ 4 = 16
+/*
+>
+<
+>=
+<=
+===
+!==
+
+*/
+age = 14;
+
+
+let maximumDataTransferSpeed: number = 23;  // 1
+let dayOfWeek: string = "monday";  // 2
+let result: number = 2 + 6 / (1 + 2);  // 3
+console.log(result);
+if (upFlag) {
+    console.log(upFlag);
+} else {
+    console.log('тест русского');
+}
+import * as readline from "readline";
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let name4: string = '5';
+r1.question('33', (name: string) => {
+    name4 = name;
+    console.log(name4);
+    r1.close();
+});
+console.log('23', name4);
 process.stdin.resume();
