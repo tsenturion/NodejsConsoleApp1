b1af63cc1f30bc859c29ec3b6d7e26ad822b9884
tsenturion <80580892+tsenturion@users.noreply.github.com>
2024-11-12 20:57:55 +0500

    input

--- script.ts
+++ script.ts
@@ -1,2 +1,16 @@
-let name2: string | null = prompt('что будет напечатано дл€ пользовател€');
-alert('hi' + name2);
+function inputNumberArray(): number[] {
+    const numbers: number[] = [];
+    let input: string | null;
+
+    do {
+        input = prompt("¬ведите число (дл€ завершени€ ввода введите пустую строку):");
+        if (input && input.trim() !== "") {
+            const num = parseFloat(input.trim());
+            if (!isNaN(num)) numbers.push(num);
+        }
+    } while (input && input.trim() !== "");
+
+    return numbers;
+}
+
+console.log("Ёлементы массива:", inputNumberArray());
