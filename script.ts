444259cbacd60242595724f0ef251fd792e67e7e
MariGA-93 <marina.muhamejanova@gmail.com>
2024-11-14 21:10:40 +0500

    2 pz done

--- script.ts
+++ script.ts
@@ -14,31 +14,38 @@ function inputNumberArray(): number[] {
 
     return numbers;
 }
+const keyNumber: { [key: string]: number } = {};
 let numbers3: number[] = inputNumberArray();
-console.log(numbers3)
-const KeyNumber: { [key: string]: number } = {};
-
-for (let i = 0; i < numbers3.length; i++) {
-    KeyNumber["key" + i.toString()] = numbers3[i]
+for (let index in numbers3) {
+    keyNumber['key' + index.toString()] = numbers3[index];
 }
-console.log(KeyNumber)
-//let numbers3: number[] = inputNumberArray();
-//for (let index in numbers3) {
-//    console.log(index);
-//    let key: string = 'key' + index.toString();
-//    console.log(key);
+//console.log(numbers3)
+//const KeyNumber: { [key: string]: number } = {};
 
+//for (let i = 0; i < numbers3.length; i++) {
+//    KeyNumber["key" + i.toString()] = numbers3[i]
 //}
-//let len = numbers3.length;
-//if (numbers3.length >= 4) {
-//    let slice1: number[] = numbers3.slice(0, 2);
-//    let slice2: number[] = numbers3.slice(len - 2, len);
-//    console.log(slice1.concat(slice2));
-//} else {
-//    let slice1: number[] = numbers3.slice(0, 1);
-//    let slice2: number[] = numbers3.slice(len - 1, len);
-//    console.log(slice1.concat(slice2));
-//}
+//console.log(KeyNumber)
+
+
+/*
+использовать функцию дл€ ввода, создать массив
+если в нем меньше 4 элементов, создать массив из 
+первого и последнего элемента и вывести его
+если в нем больше 4 элементов, создать массив из 
+первых двух и последних двух элементов и вывести его
+*/
+//https://github.com/tsenturion/NodejsConsoleApp1
+let len = numbers3.length;
+if (numbers3.length >= 4) {
+    let slice1: number[] = numbers3.slice(0, 2);
+    let slice2: number[] = numbers3.slice(len - 2, len);
+    console.log(slice1.concat(slice2));
+} else {
+    let slice1: number[] = numbers3.slice(0, 1);
+    let slice2: number[] = numbers3.slice(len - 1, len);
+    console.log(slice1.concat(slice2));
+}
 //console.log("Ёлементы массива:", numbers);
 //console.log(numbers.length); //длина массива
 //numbers.push(99, 99) //добавл€ет один или несколько элементов в конец
