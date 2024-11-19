af9e3e4d5064250bcdc7c656817418c279580abe
tsenturion <80580892+tsenturion@users.noreply.github.com>
2024-11-19 20:51:56 +0500

    testing

---
+++ app.test.ts
@@ -0,0 +1,13 @@
+import * as ta from 'type-assertions';
+
+import filterAnagrams from './app';
+
+test('function', () => {
+    expect(filterAnagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada'])).toEqual(['aabb', 'bbaa']);
+
+    expect(filterAnagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])).toEqual(['carer', 'racer']);
+
+    expect(filterAnagrams('laser', ['lazing', 'lazy', 'lacer'])).toEqual([]);
+
+    ta.assert<ta.Equal<ReturnType<typeof filterAnagrams>, string[]>>();
+});
