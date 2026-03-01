c3f3dde52cd451b6967f6323e145a7c4b77bbcf9
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-01 12:35:14 +0500

    let const

--- js.jc/01.03/main.js
+++ js.jc/01.03/main.js
@@ -1,1 +1,11 @@
 console.log('Hello, world!');
+
+let age = 30;
+console.log('Age:', age);
+
+age = 31;
+console.log('Updated Age:', age);
+
+const name = 'John';
+//name = 'Doe'; // This will cause an error because 'name' is a constant
+console.log('Name:', name);
