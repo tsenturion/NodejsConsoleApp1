31004c45ab72ed38bdf650604adbc65176e0e618
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-09 22:45:19 +0500

    npm

--- 425/app.js
+++ 425/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
