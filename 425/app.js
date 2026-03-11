9df3f77be1c8518c0cbbca37db4efc3ced201058
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-11 22:08:31 +0500

    приоритеты

--- 425/app.js
+++ 425/app.js
@@ -1,10 +1,2 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.write('ответ от сервера');
-    res.end();
-});
-
-server.listen(3000, () => {
-    console.log('сервер запущен на порту 3000');
-});
+setTimeout(() => console.log('timeout'));
+Promise.resolve().then(() => console.log('promise'));
