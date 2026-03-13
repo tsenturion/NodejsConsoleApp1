a231c47a2f0cf25723f35b9f36b958b651b65110
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-13 22:10:25 +0500

    default

--- 425/vite-project/src/main.js
+++ 425/vite-project/src/main.js
@@ -3,6 +3,9 @@ import javascriptLogo from './assets/javascript.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import { setupCounter } from './counter.js'
+import log from './logger.js'
+
+log('Hello Vite!')
 
 document.querySelector('#app').innerHTML = `
 <section id="center">
