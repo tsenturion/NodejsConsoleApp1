903f88c339483e1b47a7e49681bee806b25764d3
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-15 13:07:00 +0500

    setInterval

--- 411js.jc/01.03/main.js
+++ 411js.jc/01.03/main.js
@@ -1,5 +1,13 @@
 console.log("начало");
 setTimeout(() => {
-    console.log("внутри таймаута")
+    console.log("setTimeout")
 }, 1000);
+const timer = setInterval(() => {
+    console.log("setInterval")
+}, 500);
+
+setTimeout(() => {
+    clearInterval(timer);
+    console.log("setInterval остановлен")
+}, 3000);
 console.log("конец");
