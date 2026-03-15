9cba7c9923a12d79df0464ade0c9058cca420046
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-15 12:24:07 +0500

    querySelector querySelectorAll

--- 411js.jc/01.03/main.js
+++ 411js.jc/01.03/main.js
@@ -1,2 +1,10 @@
-const title = document.getElementById('title');
-console.log(title);
+const element = document.querySelector("p");
+/*
+document.querySelector('#title');
+document.querySelector('.menu');
+document.querySelector('div');
+*/
+console.log(element);
+
+const items = document.querySelectorAll("li");
+console.log(items);
