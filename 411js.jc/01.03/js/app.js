0dd5e3f7af5a46cba5b78d8dbe7787b9bfe6d793
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-15 14:50:37 +0500

    синтаксис

--- 411js.jc/01.03/js/app.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-  console.log("Локальная версия jQuery загружена:", $);
-
-  $("#testButton").on("click", function () {
-    $("#title").text("Локальное подключение работает");
-    $("#message").text("Файл jquery.js был успешно найден и загружен браузером.");
-  });
-});
