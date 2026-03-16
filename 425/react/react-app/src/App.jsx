a73583a81fd5308f40a61a60afeef68056452bdf
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-16 21:30:02 +0500

    горячая перезагрузка

--- 425/react/react-app/src/App.jsx
+++ 425/react/react-app/src/App.jsx
@@ -37,6 +37,8 @@ function App() {
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
+          <h2>Documentation</h2>
+
           <p>Your questions, answered</p>
           <ul>
             <li>
