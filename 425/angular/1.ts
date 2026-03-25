ea5ea0c8dbe3a0166a2cefe514c5d412286e35d4
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-25 22:30:09 +0500

    архитектура

--- 425/angular/1.ts
+++ 425/angular/1.ts
@@ -76,3 +76,72 @@ identity<string>("текст");
 export class User {}
 
 import { User } from './user';
+
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent], 
+  imports: [BrowserModule],     
+  providers: [],                
+  bootstrap: [AppComponent]     
+})
+export class AppModule {}
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>{{ title }}</h1>`,
+  styles: [`h1 { color: blue; }`]
+})
+export class AppComponent {
+  title: string = 'Angular приложение';
+}
+
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  getData(): string {
+    return 'Данные из сервиса';
+  }
+}
+
+
+import { Component } from '@angular/core';
+import { DataService } from './data.service';
+
+@Component({
+  selector: 'app-root',
+  template: `<p>{{ data }}</p>`
+})
+export class AppComponent {
+  data: string;
+
+  constructor(private dataService: DataService) {
+    this.data = this.dataService.getData();
+  }
+}
+
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `<p>Тест</p>`
+})
+export class TestComponent implements OnInit {
+  ngOnInit(): void {
+    console.log('Компонент инициализирован');
+  }
+}
+
+
