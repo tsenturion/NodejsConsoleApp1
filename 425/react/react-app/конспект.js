5ff204140aedd4f3d761998603e11489d315e3cc
Ефимов Матвей Александрович <80580892+tsenturion@users.noreply.github.com>
2026-03-27 21:07:26 +0500

    ReactiveFormsModule

--- 425/react/react-app/конспект.js
+++ 425/react/react-app/конспект.js
@@ -22,3 +22,67 @@ export class AppComponent {
 onSubmit(form: any) {
   console.log(form.value);
 }
+
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  imports: [ReactiveFormsModule]
+})
+export class AppModule {}
+
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+export class AppComponent {
+  form = new FormGroup({
+    username: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email])
+  });
+}
+
+<form [formGroup]="form" (ngSubmit)="onSubmit()">
+  <input formControlName="username">
+  <input formControlName="email">
+  <button [disabled]="form.invalid">Отправить</button>
+</form>
+
+onSubmit() {
+  console.log(this.form.value);
+}
+
+username: new FormControl('', [
+  Validators.required,
+  Validators.minLength(3)
+])
+
+<input formControlName="username">
+<div *ngIf="form.get('username')?.invalid && form.get('username')?.touched">
+  Ошибка в поле username
+</div>
+
+import { AbstractControl } from '@angular/forms';
+
+export function forbiddenName(control: AbstractControl) {
+  return control.value === 'admin' ? { forbidden: true } : null;
+}
+
+username: new FormControl('', [
+  Validators.required,
+  forbiddenName
+])
+
+
+import { FormBuilder } from '@angular/forms';
+
+constructor(private fb: FormBuilder) {}
+
+form = this.fb.group({
+  username: ['', Validators.required],
+  email: ['', Validators.email]
+});
+
+this.form.addControl('age', new FormControl(''));
+
+this.form.valueChanges.subscribe(value => {
+  console.log(value);
+});
